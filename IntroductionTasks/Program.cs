78e749a0a53f34e2c52e13336f90af4e7b609720
[Asgar Babayev] <[tu7609td3@code.edu.az]>
2022-03-03 22:41:51 +0400

    added introduction tasks

---
+++ IntroductionTasks/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace IntroductionTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1. n-dən m-ə qədər 7ə bölünən ədələrin sayını tapın.
+            //Console.Write("Başlanğıc dəyərini daxil edin: ");
+            //int firstValue = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Bitiş dəyərini daxil edin: ");
+            //int lastValue = Convert.ToInt32(Console.ReadLine());
+            //int count = 0;
+            //for (int i = firstValue; i < lastValue; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+            //Console.ReadKey();
+            #endregion
+
+            #region Task 2. Ay (string) daxil edilir. Ayın neçə gündən ibarət olduğunu deyən proqram yazın.
+            //Console.Write("Ay daxil edin: ");
+            //string month = Console.ReadLine();
+            //switch (month)
+            //{
+            //    case "yanvar":
+            //    case "mart":
+            //    case "may":
+            //    case "iyul":
+            //    case "avqust":
+            //    case "oktyabr":
+            //    case "dekabr":
+            //        Console.WriteLine("Bu ay 31 gündür.");
+            //        break;
+            //    case "aprel":
+            //    case "iyun":
+            //    case "sentyabr":
+            //    case "noyabr":
+            //        Console.WriteLine("Bu ay 30 gündür.");
+            //        break;
+            //    case "fevral":
+            //        Console.WriteLine("Bu ay 28 gündür.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Belə ay yoxdur.");
+            //        break;
+            //}
+            #endregion
+        }
+    }
+}
